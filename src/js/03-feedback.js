87afb5d158ff03b70fd1031347a94b4496191456
Nataliia Odai <natasha.oday57@gmail.com>
2022-07-22 19:37:32 +0300

    1-2-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,75 +1,90 @@
 import throttle from 'lodash.throttle';
 
 
-const inputForm = document.querySelector('.feedback-form');
-inputForm.addEventListener('input', saveFunction);
-const LOCALSTORAGE_KEY = 'feedback-form-state';
-const emailForm = inputForm.firstElementChild;
-console.log(emailForm)
-const messageForm = inputForm.lastElementChild;
-console.log(messageForm)
+// const form = document.querySelector('.feedback-form');
+// console.log(form);
+// form.addEventListener('input', saveData);
+//
+// const LOCALSTORAGE_KEY =  'feedback-form-state';
+// const email = document.querySelector("input");
+// // console.log(email)
+// const message = document.querySelector("textarea");
+// // console.log(message)
+//
+//
+// let data = localStorage.setItem(LOCALSTORAGE_KEY, form.elements.value);
+//  console.log(form.elements.value)
+//
+// // // updateOutput();
+// //
+// function saveData(evt) {
+// //     evt.preventDefault();
+// //     let saveDataObject = {};
+// //
+// //     // let data = localStorage.setItem(LOCALSTORAGE_KEY, form.elements.value);
+// //     // console.log(messageData)
+// //
+// //     // updateOutput();
+// //     // form.reset();
+//      }
+//
+// form.elements.message.value
+// form.elements.email.value
+//
+//  function updateOutput() {
+//      let getMessageData = localStorage.getItem(LOCALSTORAGE_MESSAGE_KEY) || "";
+//        console.log( getMessageData);
+//        let getEmailData = localStorage.getItem(LOCALSTORAGE_EMAIL_KEY) || "";
+//        console.log(getEmailData)
+// }
+//
+//
 
 
-function updateOutput() {
-    emailForm.textContent = localStorage.getItem(LOCALSTORAGE_KEY) || "";
-    messageForm.textContent = localStorage.getItem(LOCALSTORAGE_KEY) || "";
-}
+const CONTACT_FORM_LOCAL_STORAGE_KEY = 'formData';
+const contactForm = document.querySelector('.feedback-form');
+const formData = {};
+
+const fillContactForm = form => {
+    const formDataFromLocalStorage = JSON.parse(localStorage.getItem(CONTACT_FORM_LOCAL_STORAGE_KEY));
+    const formElements = form.elements;
+
+    for (const key in formDataFromLocalStorage) {
+        if (formDataFromLocalStorage.hasOwnProperty(key)) {
+            formElements[key].value = formDataFromLocalStorage[key];
+        }
+    }
+};
+
+fillContactForm(contactForm);
+
+const onContactFormChange = throttle(event => {
+    const { target } = event;
+
+    const contactFormValue = target.value;
+    const contactFormValueName = target.name;
+
+    formData[contactFormValueName] = contactFormValue;
+    localStorage.setItem('formData', JSON.stringify(formData));
+}, 500);
 
 
-updateOutput();
+const submitFunc = event => {
+    event.preventDefault();
+    const { email, message } = event.target;
 
-function saveFunction(evt) {
-    evt.preventDefault();
-    localStorage.setItem(LOCALSTORAGE_KEY, inputForm.elements.message.value);
-    updateOutput();
-    inputForm.reset();
-}
+    if (email.value === '' || message.value === '') {
+        return alert('Please fill in all the fields!');
+    }
+    const obj = {
+        email: contactForm.email.value,
+        message: contactForm.message.value,
+    };
 
+    console.log(obj);
+    event.currentTarget.reset();
+    localStorage.removeItem('formData');
+};
 
-// const CONTACT_FORM_LOCAL_STORAGE_KEY = 'formData';
-// const contactForm = document.querySelector('.feedback-form');
-// const formData = {};
-//
-// const fillContactForm = form => {
-//     const formDataFromLocalStorage = JSON.parse(localStorage.getItem(CONTACT_FORM_LOCAL_STORAGE_KEY));
-//     const formElements = form.elements;
-//
-//     for (const key in formDataFromLocalStorage) {
-//         if (formDataFromLocalStorage.hasOwnProperty(key)) {
-//             formElements[key].value = formDataFromLocalStorage[key];
-//         }
-//     }
-// };
-//
-// fillContactForm(contactForm);
-//
-// const onContactFormChange = throttle(event => {
-//     const { target } = event;
-//
-//     const contactFormValue = target.value;
-//     const contactFormValueName = target.name;
-//
-//     formData[contactFormValueName] = contactFormValue;
-//     localStorage.setItem('formData', JSON.stringify(formData));
-// }, 500);
-//
-//
-// const submitFunc = event => {
-//     event.preventDefault();
-//     const { email, message } = event.target;
-//
-//     if (email.value === '' || message.value === '') {
-//         return alert('Please fill in all the fields!');
-//     }
-//     const obj = {
-//         email: contactForm.email.value,
-//         message: contactForm.message.value,
-//     };
-//
-//     console.log(obj);
-//     event.currentTarget.reset();
-//     localStorage.removeItem('formData');
-// };
-//
-// contactForm.addEventListener('submit', submitFunc);
-// contactForm.addEventListener('input', throttle(onContactFormChange, 500));
+contactForm.addEventListener('submit', submitFunc);
+ contactForm.addEventListener('input', throttle(onContactFormChange, 500));
